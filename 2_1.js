e067ad1b5faea347f4ab462ee88956613c2453ef
mcostarodrigo <m.costa.rodrigo.rc@gmail.com>
2023-08-18 17:34:58 -0300

    Corrigindo

--- 2_1.js
+++ 2_1.js
@@ -1,9 +1,24 @@
-var maiorNumero= 0
-var lista = [55, 60, 10, 15]
-for(i = 0; i < lista.length; i++){
-    if(lista[i] > maiorNumero) {
-        maiorNumero = lista[i]
-    }
-}
+//var maiorNumero= 0
+//var lista = [55, 60, 10, 15]
+//for(i = 0; i < lista.length; i++){
+    //if(lista[i] > maiorNumero) {
+      //  maiorNumero = lista[i]
+    //}
+//}
 
-console.log(maiorNumero)
+//console.log(maiorNumero)
+
+const maiorNumerodoArray = [5, 8, 2, 11, 4, 9]
+
+function maiorNumerodoArray(nums){
+    let maiorNumeroAteAgora = nuns [0]
+
+    for (let i = 1; i < nums.length; i++) {
+        if(nums[i] > maiorNumeroAteAgora) {
+            maiorNumeroAteAgora= nuns[i]
+        }
+    }
+
+    return maiorNumeroAteAgora
+
+}
