8543302dac4e652467169c4679769b7f4d365f79
mcostarodrigo <m.costa.rodrigo.rc@gmail.com>
2023-08-18 15:57:45 -0300

    Atualizando

--- ex3_2.py
+++ ex3_2.py
@@ -1,4 +1,5 @@
-n = int(input('\033[1:35mDigite um número para ver sua tabuada: \033[m'))
-print(10 * '\033[1:32m-\033[m')
-print(f'{n} x  1 = {n*1} \n{n} x  2 = {n*2} \n{n} x  3 = {n*3} \n{n} x  4 = {n*4} \n{n} x  5 = {n* 5} \n{n} x 10 = {n*10}')
-print(10 * '\033[1:32m-\033[m')
+cont = 0
+x = int(input('Digite um número para ver sua tabuada: '))
+for n in range(1, 11):
+    cont += 1
+    print(f'{cont} x {x} = {cont * x}')
